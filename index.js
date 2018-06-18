ac4171400c19a14e5c325a2bc006b5aeaae97ae1
eanorton <edwardnorton@icloud.com>
2018-06-18 14:34:24 -0400

    added buttons

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const userApiUrl = "http://localhost:3000/api/v1/users"
 const noteApiUrl = "http://localhost:3000/api/v1/notes"
 
 const noteList = document.getElementById("note-list")
+const noteContent = document.getElementById("note-content")
 
 function fetchUsers() {
   fetch(userApiUrl).then(response=>response.json()).then(allUsers=>setUser(allUsers))
@@ -19,8 +20,21 @@ function setUser(allUsers){
 
 function displayNotes(allNotes){
   noteList.innerHTML = ""
-  allNotes.forEach(note=>noteList.innerHTML += `<li><h1>${note.title}</h1></li>`)
+  allNotes.forEach(note=>noteList.innerHTML += `<li><h3>${note.title}</h3><p>${note.body}</p> <button class="delete" data-note-id="${note.id}">Delete</button><button class="update" data-note-id="${note.id}">Update</button></li>`)
 }
+
+function makeNewNote(){
+
+}
+
+document.addEventListener('click', function(event){
+  event.preventDefault()
+  if (event.target.className === "update"){
+    console.log("hi")
+  } else if (event.target.className === "delete"){
+    console.log("NO")
+  }
+})
 
 fetchNotes();
 fetchUsers();
